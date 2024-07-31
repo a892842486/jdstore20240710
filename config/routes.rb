5ffdd1880676df6eb67e9676ece9fdbda25ecf52
Bamu <a892842486@github.com>
2024-07-31 11:42:57 +0800

    add ship/shipped/cancel/return action to admin/orders_controller

--- config/routes.rb
+++ config/routes.rb
@@ -29,5 +29,17 @@ Rails.application.routes.draw do
   namespace :account do
     resources :orders
   end
+
+  namespace :admin do
+    resources :products
+    resources :orders do
+      member do
+        post :cancel
+        post :ship
+        post :shipped
+        post :return
+      end
+    end
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
